cf7d14d7046e9dcce58977bbb663f1fc6e20ed55
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2023-02-22 11:05:04 +0700

    initiation 9

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -10,7 +10,7 @@ function UserDetails() {
   return (
     <>
       <div className="container d-flex flex-column align-items-center mt-5">
-        <div className="d-flex flex-column align-items-center bg-secondary w-100 rounded shadow">
+        <div className="d-flex flex-column align-items-center bg-secondary w-100 rounded shadow pt-3">
           <div style={{ width:"10rem",height:"10rem"}}>
             <img src={User.image} alt={User.username} className="w-100 rounded-circle" style={{ border:"5px solid red",width:"10rem",height:"10rem",objectFit:"cover" }}/>
           </div>
