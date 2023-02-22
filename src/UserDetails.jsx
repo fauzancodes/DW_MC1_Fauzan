b4ffbff0b8fad519568c612e8f7f6eb9b9cdf858
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2023-02-22 10:43:55 +0700

    initiation 2

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import data from "./data.json";
 
@@ -9,23 +9,24 @@ function UserDetails() {
   User = User[0];
   return (
     <>
-      <div className="container d-flex justify-content-center">
-        <div>
-          <div className="rounded-circle" style={{ width:"5rem",heiht:"5rem"}}>
+      <div className="container d-flex flex-column align-items-center">
+        <div className="d-flex flex-column align-items-center bg-secondary">
+          <div className="rounded-circle" style={{ width:"20rem",heiht:"20rem"}}>
             <img src={User.image} alt={User.username} className="w-100"/>
           </div>
           <h1>{User.username}</h1>
           <div className="d-flex">
-            <div className="me-3">
+            <div className="me-3 d-flex flex-column align-items-center">
               <p>{User.follower}</p>
               <p>Followers</p>
             </div>
-            <div>
+            <div className="d-flex flex-column align-items-center">
               <p>{User.following}</p>
               <p>Followings</p>
             </div>
           </div>
         </div>
+        <Link to="/">Back</Link>
       </div>
     </>
   );
